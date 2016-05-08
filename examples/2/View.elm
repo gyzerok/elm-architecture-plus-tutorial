831ee50089511b5d9cb1aa86bb423e4928b6c78e
Fedor Nezhivoy <gyzerok@gmail.com>
2016-05-08 11:38:00 +0600

    add isFetching logic

--- examples/2/View.elm
+++ examples/2/View.elm
@@ -11,12 +11,14 @@ import Search
 
 type alias Model =
   { search : Search.Model
+  , isFetching : Bool
   , results : List String
   }
 
 init : Model
 init =
   { search = Search.init
+  , isFetching = False
   , results = []
   }
 
@@ -50,22 +52,29 @@ update msg model =
           shouts
 
     FetchOk results ->
-      ( { model | results = results }, Cmd.none )
+      ( { model | results = results, isFetching = False }
+      , Cmd.none
+      )
 
     FetchFail ->
-      ( model, Cmd.none )
+      ( { model | isFetching = False }, Cmd.none )
 
 handleSearchShout : Search.Shout -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
 handleSearchShout shout ( model, _ ) =
   case shout of
     Search.OnSearch query ->
-      ( model, searchWiki query )
+      ( { model | isFetching = True }
+      , searchWiki query
+      )
 
 view : Model -> Html Msg
 view model =
   div []
     [ Search.view model.search |> Html.App.map MsgFromSearch
-    , ul [] (List.map resultView model.results)
+    , if model.isFetching then
+        text "Loading..."
+      else
+        ul [] (List.map resultView model.results)
     ]
 
 resultView : String -> Html msg
